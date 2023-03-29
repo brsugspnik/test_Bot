bf7940dc85b31ea85326ef31f20852697ef7d0b3
brsugspnik <tinkit95@gmail.com>
2023-03-29 12:04:11 +0800

    test open bot
    test open bot

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='[')
+#建做了一個bot 在變數bot中
+#在()中 discord呼叫bot時,會用的指令,如:  [send 123
+
+@bot.event
+#bot的事件
+async def on_ready():
+#async def funcname(parameter_list): (async是一個協程名) 
+    print(">> Bot is online <<")
+
+bot.run('MTA5MDQ3NzYzMzA1MTA0MTg2Mg.G4iGSC.anl5aYz9pwIgRtfu1cfP5yAwOpakdZB_OsnjRY')
+#token必為字串
+
